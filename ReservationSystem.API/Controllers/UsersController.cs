35922eb4e56bd6d22f1ea5b373a18459a2164a71
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2023-06-20 20:52:05 +0200

    functionalities related to user added

---
+++ ReservationSystem.API/Controllers/UsersController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using NpgsqlTypes;
+using ReservationSystem.API.Models;
+
+namespace ReservationSystem.API.Controllers
+{
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ReservationSystemContext _context;
+
+        public UsersController(ReservationSystemContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("roles")]
+        public IActionResult GetUsersAndRoles()
+        {
+            var users = _context.UsersRoles.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("reservations")]
+        public IActionResult GetReservationsPerUsers()
+        {
+            var reservations = _context.ReservationsPerUsers.ToList();
+            return Ok(reservations);
+        }
+
+        [HttpGet]
+        [Route("{id}-info")]
+        public IActionResult GetUserInfo([FromRoute]int id) 
+        {
+            var user = _context.Users.FromSqlRaw("SELECT * FROM get_user_info({0})",id);
+            return Ok(user);
+        }
+
+        [HttpPost()]
+        public IActionResult EditUser(int userId, string firstName, string lastName, string username, string password, string email)
+        {
+            var parameters = new[]
+            {
+                new NpgsqlParameter("@user_id", NpgsqlDbType.Integer) { Value = userId },
+                new NpgsqlParameter("@p_firstname", NpgsqlDbType.Varchar) { Value = firstName },
+                new NpgsqlParameter("@p_lastname", NpgsqlDbType.Varchar) { Value = lastName },
+                new NpgsqlParameter("@p_username", NpgsqlDbType.Varchar) { Value = username },
+                new NpgsqlParameter("@p_password", NpgsqlDbType.Varchar) { Value = password },
+                new NpgsqlParameter("@p_email", NpgsqlDbType.Varchar) { Value = email }
+            };
+
+            _context.Database.ExecuteSqlRaw("CALL edit_user(@user_id, @p_firstname, @p_lastname, @p_username, @p_password, @p_email)", parameters);
+
+            return Ok();
+        }
+
+        [HttpDelete("delete-{userId}")]
+        public IActionResult DeleteUser(int userId)
+        {
+            var parameter = new NpgsqlParameter("@user_id", NpgsqlDbType.Integer) { Value = userId };
+
+            _context.Database.ExecuteSqlRaw("CALL delete_user(@user_id)", parameter);
+
+            return Ok();
+        }
+
+
+    }
+}
